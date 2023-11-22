e0d8774b2355499df97d3ea0cf10bce21c75483d
Fernando E. Zimerman <fernando.ezimerman@gmail.com>
2023-11-22 18:54:58 -0300

    Pegando dados da API

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
 
 void main() {
   runApp(const HomePage());
@@ -26,12 +28,22 @@ class NavigationMain extends StatefulWidget {
 }
 
 class _NavigationState extends State<StatefulWidget> {
+  late Future<List<Book>> futureBooks;
   int currentPageIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    futureBooks = fetchBooks();
+  }
 
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = Theme.of(context);
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Book Reader'),
+      ),
       bottomNavigationBar: NavigationBar(
         onDestinationSelected: (int index) {
           setState(() {
@@ -57,120 +69,124 @@ class _NavigationState extends State<StatefulWidget> {
         ],
       ),
       body: <Widget>[
-        /// Home page
 
-        ListView.builder(
-          itemCount: 9, // 3 linhas, cada uma com 3 elementos
-          itemBuilder: (context, index) {
-            return Row(
-              children: [
-                Expanded(
-                  child: BookCard(),
-                ),
-                SizedBox(width: 8),
-                Expanded(
-                  child: BookCard(),
-                ),
-                SizedBox(width: 8),
-                Expanded(
-                  child: BookCard(),
-                ),
-              ],
-            );
+        /// Home page
+        FutureBuilder<List<Book>>(
+          future: futureBooks,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return BookCard(book: snapshot.data![index]);
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Text('${snapshot.error}');
+            }
+            // By default, show a loading spinner.
+            return const CircularProgressIndicator();
           },
         ),
 
         /// Downloaded page
 
-        ListView.builder(
-          itemCount: 9, // 3 linhas, cada uma com 3 elementos
-          itemBuilder: (context, index) {
-            return Row(
-              children: [
-                Expanded(
-                  child: BookCard(),
-                ),
-                SizedBox(width: 8),
-                Expanded(
-                  child: BookCard(),
-                ),
-                SizedBox(width: 8),
-                Expanded(
-                  child: BookCard(),
-                ),
-              ],
-            );
-          },
-        ),
-
         /// Bookmark page
-        ListView.builder(
-          itemCount: 9, // 3 linhas, cada uma com 3 elementos
-          itemBuilder: (context, index) {
-            return Row(
-              children: [
-                Expanded(
-                  child: BookCard(),
-                ),
-                SizedBox(width: 8),
-                Expanded(
-                  child: BookCard(),
-                ),
-                SizedBox(width: 8),
-                Expanded(
-                  child: BookCard(),
-                ),
-              ],
-            );
-          },
-        ),
       ][currentPageIndex],
     );
   }
 }
 
 class BookCard extends StatelessWidget {
-  const BookCard({super.key});
+  final Book book;
+
+  const BookCard({required this.book, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: 5,
-      margin: const EdgeInsets.all(8),
-      child: Column(
-        children: [
-          Image.asset(
-            'assets/images/book_cover.jpg',
-            // Substitua pelo caminho correto da imagem da capa do livro
-            height: 120,
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),
-          const Padding(
-            padding: EdgeInsets.all(8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Título do Livro',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
+        clipBehavior: Clip.hardEdge,
+        elevation: 5,
+        margin: const EdgeInsets.all(8),
+        child: InkWell(
+          splashColor: Colors.blue.withAlpha(30),
+          onTap: () {
+            debugPrint('Card tapped.');
+          },
+          child: Column(
+            children: [
+              Image.network(
+                book.coverUrl,
+                height: 120,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      book.title,
+                      style: const TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      book.author,
+                      style: const TextStyle(
+                        fontSize: 12,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
                 ),
-                SizedBox(height: 4),
-                Text(
-                  'Nome do Autor',
-                  style: TextStyle(
-                    fontSize: 12,
-                    color: Colors.grey,
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ));
+  }
+}
+
+Future<List<Book>> fetchBooks() async {
+  final response = await http.get(Uri.parse('https://escribo.com/books.json'));
+
+  if (response.statusCode == 200) {
+    // If the server returned a 200 OK response,
+    // then parse the JSON and return a list of books.
+    final List<dynamic> data = jsonDecode(response.body);
+    return data.map((item) => Book.fromJson(item)).toList();
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load books');
+  }
+}
+
+class Book {
+  final int id;
+  final String title;
+  final String author;
+  final String coverUrl;
+  final String downloadUrl;
+
+  const Book({
+    required this.id,
+    required this.title,
+    required this.author,
+    required this.coverUrl,
+    required this.downloadUrl,
+  });
+
+  factory Book.fromJson(Map<String, dynamic> json) {
+    return Book(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      author: json['author'] as String,
+      coverUrl: json['cover_url'] as String,
+      downloadUrl: json['download_url'] as String,
     );
   }
 }
